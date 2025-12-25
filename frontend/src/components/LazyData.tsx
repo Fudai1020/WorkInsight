750845fac38e9ffe9671855762febf875b546715
Fudai1020 <fuudai1020@gmail.com>
2025-12-26 08:58:26 +0900

    add Lazydata layouts

--- frontend/src/components/LazyData.tsx
+++ frontend/src/components/LazyData.tsx
@@ -1,7 +1,22 @@
+import LazyNextData from "./LazyNextData"
+import LazyTodayData from "./LazyTodayData"
+import Timer from "./Timer"
 
 const LazyData = () => {
   return (
-    <div className="border rounded-xl w-full h-full">LazyData</div>
+    <div className="border rounded-xl w-full h-full">
+      <div className="grid grid-cols-[1fr_1.5fr_1fr] h-full gap-6">
+        <div className="border-r">
+          <LazyTodayData />
+        </div>
+        <div className="flex items-center justify-center">
+        <Timer />
+        </div>
+        <div className="border-l   ">
+          <LazyNextData />
+        </div>
+      </div>
+    </div>
   )
 }
 
