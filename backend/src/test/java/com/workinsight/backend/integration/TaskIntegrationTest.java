b525c37165ad4530251e215ea12236da03fb2506
Fudai1020 <fuudai1020@gmail.com>
2026-02-03 23:41:56 +0900

    add taskForm and build test

--- backend/src/test/java/com/workinsight/backend/integration/TaskIntegrationTest.java
+++ backend/src/test/java/com/workinsight/backend/integration/TaskIntegrationTest.java
@@ -1,9 +1,9 @@
 package com.workinsight.backend.integration;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,6 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.jayway.jsonpath.JsonPath;
 
 import jakarta.transaction.Transactional;
 
@@ -28,7 +31,7 @@ public class TaskIntegrationTest extends UserIntegrationTest{
         registerUser(email, password);
         String token = loginAndGetToken(email,password);
         mockMvc.perform(
-            post("/api/tasks/createTask")
+            post("/api/tasks")
                 .header("Authorization", "Bearer " + token)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("""
@@ -46,5 +49,45 @@ public class TaskIntegrationTest extends UserIntegrationTest{
         .andExpect(jsonPath("$.taskPriority").value("MEDIUM"))
         .andExpect(jsonPath("$.taskDeadline").value("2026-01-30"))
         .andExpect(jsonPath("$.taskMemo").value("メモ"));
+    }
+    @Test
+    void タスクを更新できる()throws Exception{
+        String email = "test@example.com";
+        String password = "password";
+        registerUser(email, password);
+        String token = loginAndGetToken(email, password);
+        MvcResult createResult =  mockMvc.perform(
+            post("/api/tasks")
+                .header("Authorization", "Bearer "+token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                    {
+                        "taskTitle":"タスク",
+                        "taskPriority":"MEDIUM",
+                        "taskDeadline":"2026-01-30",
+                        "taskMemo":"メモ"
+                    }
+                        """)
+        )
+        .andExpect(status().isOk())
+        .andReturn();
+        Number taskId = JsonPath.read(createResult.getResponse().getContentAsString(),"$.taskId");
+        Long taskIdLong = taskId.longValue();
+        mockMvc.perform(
+            put("/api/tasks/"+taskIdLong)
+                .header("Authorization","Bearer "+token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "taskTitle":"更新後タスク",
+                            "taskPriority":"HIGH",
+                            "taskStatus":"DONE"
+                        }
+                        """)
+        )
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.taskTitle").value("更新後タスク"))
+        .andExpect(jsonPath("$.taskPriority").value("HIGH"))
+        .andExpect(jsonPath("$.taskStatus").value("DONE"));
     }
 }
