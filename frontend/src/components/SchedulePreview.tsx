b25dd1735c150fbf3ef53f3a9230e8e1bdf0dd13
Fudai1020 <fuudai1020@gmail.com>
2026-02-15 15:30:44 +0900

    integration get dashboard tasks and schedules

--- frontend/src/components/SchedulePreview.tsx
+++ frontend/src/components/SchedulePreview.tsx
@@ -1,13 +1,10 @@
 import { useModal } from "../context/ModalContext"
+type Props = {
+  schedules:any[];
+}
 
-
-const SchedulePreview = () => {
+const SchedulePreview = ({schedules}:Props) => {
   const {openModal} = useModal();
-  const schedules = [
-    {id:1,title:"終日イベント",allDay:true,startTime:null,endTime:null},
-    {id:2,title:"定例MTG",allDay:false,startTime:"10:00",endTime:"10:30"},
-    {id:3,title:"会議",allDay:false,startTime:"13:00",endTime:"13:30"},
-  ];
   const sortedSchedules = [...schedules].sort((a,b) => {
     if(a.allDay && !b.allDay) return -1;
     if(!a.allDay && b.allDay) return 1;
@@ -19,18 +16,21 @@ const SchedulePreview = () => {
       <div className="flex flex-col items-center h-full">
         <h1 className="text-3xl font-semibold mt-5">今日のスケジュール</h1>
         <ul className="w-[80%] flex flex-col gap-4 mt-7 overflow-y-auto">
-          {sortedSchedules.map(s => (
+          {schedules.length === 0 ? (
+            <span className="text-center text-2xl">本日の予定はありません</span>
+          ):(
+          sortedSchedules.map(s => (
             <li key={s.id} className="grid grid-cols-[6rem_1fr] items-center gap-30">
               <span className="text-xl whitespace-nowrap">
                 {s.allDay ? "終日":`${s.startTime}〜${s.endTime}`}
               </span>
               <span className="text-2xl truncate">{s.title}</span>
             </li>
-          ))}
+          )))}
         </ul>
         <div className="border-t mt-auto pb-6 pt-5 w-[80%]  text-center">
           <span className="text-xl underline hover:scale-[1.05] cursor-pointer"
-                onClick={()=>openModal("schedule",)}>＋予定の追加</span>
+                onClick={()=>openModal("schedule",{data:schedules})}>＋予定の追加</span>
         </div>
       </div>
     </div>
--- frontend/src/components/TaskPreview.tsx
+++ frontend/src/components/TaskPreview.tsx
@@ -1,29 +1,30 @@
 
 import { useModal } from "../context/ModalContext";
-
-const TaskPreview = () => {
+type Props = {
+  tasks:any[];
+}
+const TaskPreview = ({tasks}:Props) => {
   const {openModal} = useModal();
-  const mockTasks = [
-    {id:1,title:'タスク１',priority:'高'},
-    {id:2,title:'タスク２',priority:'中'},
-    {id:3,title:'タスク３',priority:'低'},
-  ];
+
   return (
     <div className="border rounded-xl w-full h-full">
       <div className="flex flex-col items-center h-full gap-10">
         <h1 className="text-3xl font-semibold mt-5">今日のタスク</h1>
         <ul className="flex flex-col gap-2 overflow-y-auto">
-          {mockTasks.map(item => (
-            <li key={item.id} className="grid grid-cols-[auto_1fr_auto] gap-15 items-center">
-              <input type="checkbox" className="size-5"/>
-              <span className="text-2xl">{item.title}</span>
-              <span className="text-2xl">{item.priority}</span>
-            </li>
-          ))}
+          {tasks.length === 0 ? (
+            <span className="text-2xl">本日のタスクはありません</span>):
+            (tasks.map(item => (
+              <li key={item.id} className="grid grid-cols-[auto_1fr_auto] gap-15 items-center">
+                <input type="checkbox" className="size-5"/>
+                <span className="text-2xl">{item.title}</span>
+                <span className="text-2xl">{item.priority}</span>
+              </li>
+          )))
+          }
         </ul>
         <div className="border-t mt-auto pb-6 pt-5 w-[80%]  text-center">
           <span className="text-xl underline hover:scale-[1.05 cursor-pointer]"
-                onClick={()=>openModal("task",{data:mockTasks})}>＋タスクの追加</span>
+                onClick={()=>openModal("task",{data:tasks})}>＋タスクの追加</span>
         </div>
       </div>
     </div>
