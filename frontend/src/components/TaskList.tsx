42d55e0be7e59332e7ac28881999cfd07741bd5d
Fudai1020 <fuudai1020@gmail.com>
2026-02-27 08:22:10 +0900

    integrate task-page and update

--- frontend/src/components/TaskList.tsx
+++ frontend/src/components/TaskList.tsx
@@ -1,39 +1,53 @@
 import { useModal } from "../context/ModalContext";
-
+type Task = {
+    taskId:number;
+    taskTitle:string;
+    taskDeadline:string|null;
+    taskPriority:string;
+}
 type props = {
-tasks:any[];
+tasks:Task[];
 onSelectedId:(id:number)=>void;
 selectedTaskId:number|null;
 }
-const TaskList = ({tasks,onSelectedId,selectedTaskId}:props) => {
+const TaskList = ({tasks,onSelectedId,selectedTaskId}:props) => {    
     const {openModal} = useModal();
+    const isOverdue = (deadline:string | null) =>{
+        if(!deadline) return false;
+        const today = new Date();
+        const todayOnly = new Date(today.getFullYear(),today.getMonth(),today.getDate());
+        const  deadlineDate = new Date(deadline);
+        return deadlineDate < todayOnly;
+    }
   return (
     <div className="flex flex-col h-full gap-3">
         <div className="flex-1 overflow-y-auto  p-3 ">
             <span className="text-3xl">期限なし</span>
             <ul className="my-2 pl-5 flex flex-col gap-5">
-                {tasks.filter(t => !t.deadline).map(t=>(
-                    <li key={t.id} className="text-xl border-b pb-2 "
-                        onClick={()=>onSelectedId(t.id)}>
-                        <div className={`flex gap-3 p-2 rounded-md ${selectedTaskId === t.id ? "bg-gray-200":"hover:bg-gray-200"}`}>
+                {tasks.filter(t => !t.taskDeadline).map(t=>(
+                    <li key={t.taskId} className="text-xl border-b pb-2 "
+                        onClick={()=>onSelectedId(t.taskId)}>
+                        <div className={`flex gap-3 p-2 rounded-md ${selectedTaskId === t.taskId ? "bg-gray-200":"hover:bg-gray-200"}`}>
                             <input type="checkbox" className="scale-125"/>
-                                {t.title}
+                                {t.taskTitle}
                         </div>
                     </li>
                 ))}
             </ul>
             <span className="text-3xl">期限あり</span>
             <ul className="my-2 pl-5 flex flex-col gap-5">
-                {tasks.filter(t => t.deadline).map(t=>(
-                <li key={t.id} className="text-xl border-b pb-2"
-                    onClick={()=>onSelectedId(t.id)}>
-                    <div className={`flex gap-3 p-2 rounded-md ${selectedTaskId === t.id ? "bg-gray-200":"hover:bg-gray-200"}`}>
+                {tasks.filter(t => t.taskDeadline).map(t=>{
+                    const overdue = isOverdue(t.taskDeadline);
+                    return(
+                <li key={t.taskId} className="text-xl border-b pb-2"
+                    onClick={()=>onSelectedId(t.taskId)}>
+                    <div className={`flex gap-3 p-2 rounded-md ${selectedTaskId === t.taskId ? "bg-gray-200":"hover:bg-gray-200"}`}>
                         <input type="checkbox" className="scale-125"/>
-                        <span>{t.title}</span> 
-                        <span className="ml-auto mt-auto text-base">{t.deadline}</span>
+                        <span className={`${overdue ? "text-red-500":"text-black"}`}>{t.taskTitle}</span> 
+                        <span className={`ml-auto mt-auto text-base ${overdue ? "text-red-500":"text-black"}`}>{t.taskDeadline}</span>
                     </div>
                 </li>
-                ))}
+                )})}
             </ul>
         </div>
 
