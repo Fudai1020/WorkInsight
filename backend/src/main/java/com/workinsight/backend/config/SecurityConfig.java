23449377b0ddda7b5bd273606c1bf9f16f32700d
Fudai1020 <fuudai1020@gmail.com>
2026-03-31 21:29:12 +0900

    fix cors security

--- backend/src/main/java/com/workinsight/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/workinsight/backend/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://work-insight.vercel.app"));
         config.setAllowedMethods(List.of("POST","GET","PUT","DELETE","OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
